e89909426446f9ae607d5fbe62390874e6fd46f9
Eduardo Tenório <embatbr@gmail.com>
2015-09-21 01:19:37 -0300

    Insertion sort in asc and desc order. Linear search.
    Signed-off-by: Eduardo Tenório <embatbr@gmail.com>

--- algorithms-and-data-structures/codes/src/insertion_sort.c
+++ algorithms-and-data-structures/codes/src/insertion_sort.c
@@ -13,9 +13,9 @@ void print_sequence(int sequence[], int length)
 }
 
 
-void insertion_sort()
+void insertion_sort_asc()
 {
-    int sequence[LENGTH] = {5, 2, 4, 6, 1, 3};
+    int sequence[LENGTH] = {31, 41, 59, 26, 41, 58};
     print_sequence(sequence, LENGTH);
 
     int j;
@@ -35,8 +35,60 @@ void insertion_sort()
     print_sequence(sequence, LENGTH);
 }
 
+void insertion_sort_desc()
+{
+    int sequence[LENGTH] = {31, 41, 59, 26, 41, 58};
+    print_sequence(sequence, LENGTH);
+
+    int j;
+    for(j = 1; j < LENGTH; j++)
+    {
+        int key = sequence[j];
+        int i = j - 1;
+        while((i >= 0) && (sequence[i] < key))
+        {
+            sequence[i + 1] = sequence[i];
+            i--;
+        }
+
+        sequence[i + 1] = key;
+    }
+
+    print_sequence(sequence, LENGTH);
+}
+
+void linear_search(int key)
+{
+    int sequence[LENGTH] = {31, 41, 59, 26, 41, 58};
+    printf("searching for key %d in ", key);
+    print_sequence(sequence, LENGTH);
+
+    int i, pos;
+    for(i = 0, pos = -1; i < LENGTH && pos < 0; i++)
+    {
+        if(sequence[i] == key)
+        {
+            pos = i;
+        }
+    }
+
+    if(pos < 0)
+    {
+        printf("key %d was not found\n", key);
+    }
+    else
+    {
+        printf("key %d was found in position %d\n", key, pos);
+    }
+}
+
 
 int main()
 {
-    insertion_sort();
+    printf("asc order:\n");
+    insertion_sort_asc();
+    printf("desc order:\n");
+    insertion_sort_desc();
+    linear_search(59);
+    linear_search(10);
 }
